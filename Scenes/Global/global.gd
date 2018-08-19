e99d0c615410f78cece7a2460db886498b3fb368
PanTomko <pantomko@gmail.com>
2018-08-19 19:09:14 +0200

    Some experiments
    mosly threads/mutex etc.

---
+++ Scenes/Global/global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# Here will teak place of :
+# - loading levels
+# - bringing / removing Menu
+# - handling puse
+# - teak care of all time loded data ( player )
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
---
+++ loading.gd
@@ -0,0 +1,24 @@
+extends Node
+
+onready var T1 = Thread.new()
+
+signal on_load_done
+
+func _ready():
+	connect( "on_load_done", self, "set_load")
+	T1.start(self, "load_long")
+	print("Noice1")
+
+func load_long( data ):
+	yield( get_tree().create_timer(5.0), "timeout")
+	print("Noice2")
+	#emit_signal("on_load_done")
+	pass
+
+func set_load():
+	print("Noice")
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
